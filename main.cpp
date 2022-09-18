524b563d2de10bcda328d21b4c4d6948adec6e9c
Erik <erikr.ramsey@gmail.com>
2022-09-18 03:03:34 -0700

    Turns out player handle order matters

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ GGPOSession* ggpo;
 GGPOErrorCode result;
 GGPOPlayer players[MAX_PLAYERS];
 GGPOPlayerHandle player_handles[MAX_PLAYERS];
-
-char ip[MAX_PLAYERS][20] = { {0} };
-int  port[MAX_PLAYERS] = { 0 };
+int localPort;
+int localPlayer;
 
 void startGGPOSession(int numPlayers);
+void advance_game_state(int inputs[MAX_PLAYERS]);
 void endGGPOSession();
 void mainLoop();
 
@@ -39,12 +39,21 @@ int main(int argc, char** argv) {
 	WSADATA wd = { 0 };
 	WSAStartup(MAKEWORD(2, 2), &wd);
 
-	int numPlayers = 1;
-	port[0] = atoi(argv[1]);
-	strcpy(ip[0], "127.0.0.1");
-	for (int i = 2; i < argc; i += 2) {
-		port[i / 2] = atoi(argv[i]);
-		strcpy(ip[i / 2], argv[i + 1]);
+	int numPlayers = 0;
+	for (int a = 1; a < argc; a += 2) {
+		int i = a / 2;
+		players[i].size = sizeof(players[i]);
+		players[i].player_num = i + 1;
+		if (strcmp(argv[a + 1], "local") == 0) {
+			players[i].type = GGPO_PLAYERTYPE_LOCAL;
+			localPort = atoi(argv[a]);
+		} else {
+			players[i].type = GGPO_PLAYERTYPE_REMOTE;
+			strcpy(players[i].u.remote.ip_address, argv[a + 1]);
+			players[i].u.remote.port = atoi(argv[a]);
+		}
+
+
 		numPlayers++;
 	}
 
@@ -58,19 +67,18 @@ int main(int argc, char** argv) {
 void mainLoop() {
 	int inputs[MAX_PLAYERS] = { 0 };
 	int disconnect_flags;
-	std::cout << "Listening on port: " << port[0] << std::endl;
+	std::cout << "Listening on port: " << localPort << std::endl;
 	int i = 0;
 	srand(timeGetTime());
 	while (true) {
 		int input = 0;
 		if (i % 50 == 0)
 			input = (rand() % 2) - 1;
-		result = ggpo_add_local_input(ggpo, player_handles[0], &input, sizeof(input));
+		result = ggpo_add_local_input(ggpo, player_handles[localPlayer], &input, sizeof(input));
 		if (GGPO_SUCCEEDED(result)) {
 			result = ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * MAX_PLAYERS, &disconnect_flags);
 			if (GGPO_SUCCEEDED(result)) {
-				gameState.position1 += inputs[0];
-				gameState.position2 += inputs[1];
+				advance_game_state(inputs);
 				ggpo_advance_frame(ggpo);
 			}
 		}
@@ -81,6 +89,12 @@ void mainLoop() {
 	}
 }
 
+void advance_game_state(int inputs[MAX_PLAYERS]) {
+	return;
+	gameState.position1 += inputs[0];
+	gameState.position2 += inputs[1];
+}
+
 void startGGPOSession(int numPlayers) {
 	cb.begin_game = begin_game_cb;
 	cb.advance_frame = advance_frame_cb;
@@ -89,27 +103,20 @@ void startGGPOSession(int numPlayers) {
 	cb.free_buffer = free_buffer_cb;
 	cb.on_event = on_event_cb;
 
-	result = ggpo_start_session(&ggpo, &cb, "SimpleNetGame", numPlayers, sizeof(int), port[0]);
+	result = ggpo_start_session(&ggpo, &cb, "SimpleNetGame", numPlayers, sizeof(int), localPort);
 	if (result)	throw std::exception("Failed to start ggpo session");
 
 	ggpo_set_disconnect_timeout(ggpo, 3000);
 	ggpo_set_disconnect_notify_start(ggpo, 1000);
 
-	players[0].size = sizeof(players[0]);
-	players[0].type = GGPO_PLAYERTYPE_LOCAL;
-	players[0].player_num = 1;
 	for (int i = 1; i < MAX_PLAYERS; i++) {
-		players[i].size = sizeof(players[i]);
-		players[i].player_num = i + 1;
-		players[i].type = GGPO_PLAYERTYPE_REMOTE;
-		strcpy(players[i].u.remote.ip_address, ip[i]);
-		players[i].u.remote.port = port[i];
 	}
 
 	for (int i = 0; i < numPlayers; i++) {
 		std::cout << "Adding player: " << players[i].u.remote.ip_address << ':'
 				  << players[i].u.remote.port << std::endl;
 		result = ggpo_add_player(ggpo, &players[i], &player_handles[i]);
+		if (players[i].type == GGPO_PLAYERTYPE_LOCAL) localPlayer = i;
 		if (result) throw std::exception("Error creating player");
 	}
 }
@@ -126,8 +133,7 @@ bool advance_frame_cb(int flags) {
 	int inputs[MAX_PLAYERS] = { 0 };
 	int disconnect_flags;
 	ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * MAX_PLAYERS, &disconnect_flags);
-	gameState.position1 += inputs[0];
-	gameState.position2 += inputs[1];
+	advance_game_state(inputs);
 	ggpo_advance_frame(ggpo);
 
 	return true;
@@ -151,7 +157,6 @@ void free_buffer_cb(void* buffer) {
 }
 
 bool on_event_cb(GGPOEvent* info) {
-	int progress;
 	switch (info->code) {
 	case GGPO_EVENTCODE_CONNECTED_TO_PEER:
 		std::cout << "Connected to peer: " << info->u.connected.player << std::endl;
@@ -172,9 +177,7 @@ bool on_event_cb(GGPOEvent* info) {
 		std::cout << "Connection resumed" << std::endl;
 		break;
 	case GGPO_EVENTCODE_DISCONNECTED_FROM_PEER:
-		std::cout << "Player Disconnected" << std::endl;
-		endGGPOSession();
-		startGGPOSession(3);
+		std::cout << "Player Disconnected: " << info->u.disconnected.player << std::endl;
 		break;
 	case GGPO_EVENTCODE_TIMESYNC:
 		std::cout << "Timesync" << std::endl;
