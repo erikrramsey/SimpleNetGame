2e0422e2d7ff3c7bd0c0928ff02b96113fa4e732
Erik <erikr.ramsey@gmail.com>
2022-09-18 01:00:47 -0700

    adding support for up to 6 players

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 ﻿#include <iostream>
 #include <exception>
+#include <random>
 
 #include <Windows.h>
 #include "vendor/ggponet.h"
 
+const int MAX_PLAYERS = 6;
 GGPOSession* ggpo;
 GGPOErrorCode result;
-GGPOPlayer p1, p2;
-GGPOPlayerHandle player_handles[2];
+GGPOPlayer players[MAX_PLAYERS];
+GGPOPlayerHandle player_handles[MAX_PLAYERS];
 
-int localPort;
-int remotePort;
-char ip[20];
+char ip[MAX_PLAYERS][20] = { {0} };
+int  port[MAX_PLAYERS] = { 0 };
 
-void startGGPOSession();
+void startGGPOSession(int numPlayers);
 void endGGPOSession();
 void mainLoop();
 
@@ -21,6 +22,9 @@ struct GameState {
 	int position1;
 	int position2;
 } gameState;
+
+struct SessionState {
+} sessionState;
 
 GGPOSessionCallbacks cb;
 bool begin_game_cb(const char* game);
@@ -33,48 +37,51 @@ bool on_event_cb(GGPOEvent * info);
 
 int main(int argc, char** argv) {
 	WSADATA wd = { 0 };
-	wchar_t wide_ip_buffer[128];
-	unsigned int wide_ip_buffer_size = (unsigned int)ARRAYSIZE(wide_ip_buffer);
 	WSAStartup(MAKEWORD(2, 2), &wd);
 
-	localPort = atoi(argv[1]);
-	remotePort = atoi(argv[2]);
-	strcpy(ip, argv[3]);
+	int numPlayers = 1;
+	port[0] = atoi(argv[1]);
+	strcpy(ip[0], "127.0.0.1");
+	for (int i = 2; i < argc; i += 2) {
+		port[i / 2] = atoi(argv[i]);
+		strcpy(ip[i / 2], argv[i + 1]);
+		numPlayers++;
+	}
 
-	startGGPOSession();
+	startGGPOSession(numPlayers);
 	mainLoop();
 	endGGPOSession();
 	return 0;
 }
 
+
 void mainLoop() {
-	int inputs[2] = { 0 };
+	int inputs[MAX_PLAYERS] = { 0 };
 	int disconnect_flags;
-	int cms;
-	std::cout << "Listening on port: " << localPort << std::endl;
+	std::cout << "Listening on port: " << port[0] << std::endl;
 	int i = 0;
+	srand(timeGetTime());
 	while (true) {
-		cms = timeGetTime();
 		int input = 0;
-		if (i % 10 == 0)
-			input = rand();
+		if (i % 50 == 0)
+			input = (rand() % 2) - 1;
 		result = ggpo_add_local_input(ggpo, player_handles[0], &input, sizeof(input));
-		if (!result) {
-			result = ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * 2, &disconnect_flags);
-			if (!result) {
-				std::cout << inputs[0] << ' ' << inputs[1] << std::endl;
+		if (GGPO_SUCCEEDED(result)) {
+			result = ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * MAX_PLAYERS, &disconnect_flags);
+			if (GGPO_SUCCEEDED(result)) {
+				gameState.position1 += inputs[0];
+				gameState.position2 += inputs[1];
 				ggpo_advance_frame(ggpo);
 			}
 		}
 		
 		ggpo_idle(ggpo, 100);
 		Sleep(20);
-		double fps = 1000.0 / (timeGetTime() - cms);
 		i++;
 	}
 }
 
-void startGGPOSession() {
+void startGGPOSession(int numPlayers) {
 	cb.begin_game = begin_game_cb;
 	cb.advance_frame = advance_frame_cb;
 	cb.load_game_state = load_game_state_cb;
@@ -82,26 +89,29 @@ void startGGPOSession() {
 	cb.free_buffer = free_buffer_cb;
 	cb.on_event = on_event_cb;
 
-	result = ggpo_start_session(&ggpo, &cb, "SimpleNetGame", 2, sizeof(int), localPort);
+	result = ggpo_start_session(&ggpo, &cb, "SimpleNetGame", numPlayers, sizeof(int), port[0]);
 	if (result)	throw std::exception("Failed to start ggpo session");
 
+	ggpo_set_disconnect_timeout(ggpo, 3000);
+	ggpo_set_disconnect_notify_start(ggpo, 1000);
 
-	p1.size = sizeof(p1);
-	p1.type = GGPO_PLAYERTYPE_LOCAL;
-	p1.player_num = 1;
-	p2.size = sizeof(p2);
-	p2.type = GGPO_PLAYERTYPE_REMOTE;
-	p2.player_num = 2;
-	strcpy(p2.u.remote.ip_address, ip);
-	p2.u.remote.port = remotePort;
+	players[0].size = sizeof(players[0]);
+	players[0].type = GGPO_PLAYERTYPE_LOCAL;
+	players[0].player_num = 1;
+	for (int i = 1; i < MAX_PLAYERS; i++) {
+		players[i].size = sizeof(players[i]);
+		players[i].player_num = i + 1;
+		players[i].type = GGPO_PLAYERTYPE_REMOTE;
+		strcpy(players[i].u.remote.ip_address, ip[i]);
+		players[i].u.remote.port = port[i];
+	}
 
-	result = ggpo_add_player(ggpo, &p1, &player_handles[0]);
-	if (result) throw std::exception("Error creating local player");
-
-	result = ggpo_add_player(ggpo, &p2, &player_handles[1]);
-	if (result) throw std::exception("Error creating remote player");
-
-
+	for (int i = 0; i < numPlayers; i++) {
+		std::cout << "Adding player: " << players[i].u.remote.ip_address << ':'
+				  << players[i].u.remote.port << std::endl;
+		result = ggpo_add_player(ggpo, &players[i], &player_handles[i]);
+		if (result) throw std::exception("Error creating player");
+	}
 }
 
 void endGGPOSession() {
@@ -113,9 +123,11 @@ bool begin_game_cb(const char* game) {
 }
 
 bool advance_frame_cb(int flags) {
-	int inputs[2] = { 0 };
+	int inputs[MAX_PLAYERS] = { 0 };
 	int disconnect_flags;
-	ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * 2, &disconnect_flags);
+	ggpo_synchronize_input(ggpo, (void*)inputs, sizeof(int) * MAX_PLAYERS, &disconnect_flags);
+	gameState.position1 += inputs[0];
+	gameState.position2 += inputs[1];
 	ggpo_advance_frame(ggpo);
 
 	return true;
@@ -142,7 +154,7 @@ bool on_event_cb(GGPOEvent* info) {
 	int progress;
 	switch (info->code) {
 	case GGPO_EVENTCODE_CONNECTED_TO_PEER:
-		std::cout << "Connected" << std::endl;
+		std::cout << "Connected to peer: " << info->u.connected.player << std::endl;
 		break;
 	case GGPO_EVENTCODE_SYNCHRONIZING_WITH_PEER:
 		std::cout << "Synchronizing" << std::endl;
@@ -160,10 +172,13 @@ bool on_event_cb(GGPOEvent* info) {
 		std::cout << "Connection resumed" << std::endl;
 		break;
 	case GGPO_EVENTCODE_DISCONNECTED_FROM_PEER:
-		std::cout << "Disconnected" << std::endl;
+		std::cout << "Player Disconnected" << std::endl;
+		endGGPOSession();
+		startGGPOSession(3);
 		break;
 	case GGPO_EVENTCODE_TIMESYNC:
-		std::cout << "Timesynce" << std::endl;
+		std::cout << "Timesync" << std::endl;
+		Sleep(1000 * info->u.timesync.frames_ahead / 60);
 		break;
 	}
 	return true;
